2f8eea21f55e1091af9048010a5bb8214983b38d
HaudegenHH <einsundeins79@gmx.de>
2023-06-10 13:52:44 +0200

    horizontal scrollbar fixed

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -91,7 +91,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
         <HorizontalScrollbar 
           data={bodyParts} 
           bodyPart={bodyPart}
-          setBodyPart 
+          setBodyPart={setBodyPart}
         />
       </Box>
     </Stack>
